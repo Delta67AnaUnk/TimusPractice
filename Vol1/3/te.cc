8f1e340dcb7a84e1e1f832b09fee9f0e57f6b073
Delta67AnaUnk <analysisunknown@gmail.com>
2016-03-06 21:56:52 +0000

    1-3-0.04 C++, trie & dp, time limit #10

--- Vol1/3/te.cc
+++ Vol1/3/te.cc
@@ -1,20 +1,10 @@
-/*
- * Algorithm:
- * Trie Tree: save the wordlist using a trie tree with the related number as key
- * a visit on root at any time cause the count to increase
- *
- * DP:
- * When reaches a node containing word and is not a leaf node, two branches are 
- * generated, the one that goes back to root increases the count number. DFT, and,
- * when reach the end, save the whole path and, for every char in the array, save 
- * the current best solution
- * 
- */
 #include <iostream>
 #include <string>
 #include <map>
 #include <vector>
 #include <list>
+
+//#define DEBUG
 
 using namespace std;
 
@@ -27,13 +17,12 @@ struct TrieNode{
 
 	TrieNode(TrieNode* root):children(),root(root),word(){}
 	static TrieNode* buildTree(list<string>& wordlist);
-	static TrieNode* buildRevTree(list<string>& wordlist);
 	void flush();
 	TrieNode* getChild(char key){ return children[key];}
-	void addChild(char key){if(children[key]==NULL)	children[key] = new TrieNode(this->root);}
 	TrieNode* operator[](const char key){return children[key];}
-	bool isLeaf(){return children.size()==0;}
-	bool hasKey(char ch){return children.find(ch)!=children.end();}
+	bool hasKey(char key){return children.find(key)!=children.end();}
+	bool hasWord() const{return !word.empty();}
+	void addChild(char key){if(!hasKey(key)) children[key] = new TrieNode(this->root);}
 };
 
 TrieNode* TrieNode::buildTree(list<string>& wordlist)
@@ -41,30 +30,12 @@ TrieNode* TrieNode::buildTree(list<string>& wordlist)
 	TrieNode* root;
 	TrieNode* cur;
 	root = new TrieNode(root);
+	root->root = root;
 	auto it = wordlist.begin();
 	for(it;it != wordlist.end();it++){
 		cur = root;
 		auto itc = it->begin();
 		for(itc;itc != it->end();itc++){
-			char ch = maptable[*itc];
-			cur->addChild(ch);
-			cur = (*cur)[ch];
-		}
-		cur->word = *it;
-	}
-	return root;
-}
-
-TrieNode* TrieNode::buildRevTree(list<string>& wordlist)
-{
-	TrieNode* root;
-	TrieNode* cur;
-	root = new TrieNode(root);
-	auto it = wordlist.begin();
-	for(it;it != wordlist.end();it++){
-		cur = root;
-		auto itc = it->rbegin();
-		for(itc;itc != it->rend();itc++){
 			char ch = maptable[*itc];
 			cur->addChild(ch);
 			cur = (*cur)[ch];
@@ -83,141 +54,202 @@ void TrieNode::flush()
 	delete this;
 }
 
-struct Memo{
-	list<string> str;
-	TrieNode* ptr,*rptr;
+struct BestNode{
+	int count;
+	bool rollback;
 
-	Memo(TrieNode* ptr, TrieNode* rptr):str(),ptr(ptr),rptr(rptr){}
-	Memo():str(),ptr(nullptr),rptr(nullptr){}
-	Memo(const Memo& other):str(other.str),ptr(other.ptr),rptr(other.ptr){}
-	Memo* next(const char ch);
-	Memo* rnext(const char ch);
-	void deact(){str.clear();ptr = nullptr; rptr = nullptr;}
-	bool operator <(const Memo& other){return this->str.size() < other.str.size() && this->str.size() > 0;}
+	BestNode():count(-10),rollback(false){}
+	bool valid() const{return count > -1;}
 };
 
-Memo* Memo::next(const char ch)
+#ifndef DEBUG
+int FindPart(string& call,string::size_type start,TrieNode* cur, vector<BestNode>& best)
 {
-	// pointer always points on the current node (thus root is never reached)
-	if(ptr==nullptr) return nullptr;
-	// cases:
-	// 1. ptr has string, no child 
-	// 	1.1 root has child -> save current string and push new tail
-	// 	1.2 root has no child -> dead branch
-	// 2. ptr has no string, no child -> dead branch
-	// 3. ptr has child -> update tail
-	Memo* ret = nullptr;
-	if( ptr->hasKey(ch) ){ // child exists
-		ret = new Memo(*this);
-		ret->ptr = (*ptr)[ch];
-		ret->str.back() = ptr->word;
-	} else {
-		if(str.back().empty()) // no string, dead
-			return nullptr;
-		else{
-			if( ptr->root->hasKey(ch) ){
-				ret = new Memo(*this);
-				ret.ptr = (*(ptr->root))[ch];
-				ret.str.push_back(ptr->word);
+	string::size_type i;
+	TrieNode* tmpnode;
+	for(i = start;i < call.size()+1;i++){
+		char ch = 0;
+		if(i < call.size()) ch = call[i];
+		// 1. has child, has word, root also has child: two branches
+		// 2. no child, has word: go back to root and try
+		// 	2.1 root has child: continue
+		// 	2.2 root has no child: dead branch, same as 3
+		// 3. no child, no word: dead branch, block the entrance of this line in the previous node
+		// 4. has child, no word: continue
+		if(cur->hasWord()){
+			// the cur pointer here still points to i-1
+			if(i!=call.size() && best[i].valid()){
+				return best[i].count+1;
+			}else if(i==call.size()){
+				return 1;
 			}
-			else
-				return nullptr;
+			BestNode b1,b2;
+			if(cur->hasKey(ch)){
+				tmpnode = (*cur)[ch];
+				b1.rollback = false;
+				b1.count = FindPart(call,i+1,tmpnode,best);
+			}
+			if(cur->root->hasKey(ch)){
+				tmpnode = (*(cur->root))[ch];
+				b2.rollback = true;
+				b2.count = FindPart(call,i+1,tmpnode,best)+1;
+			}
+			if(b1.valid() && !b2.valid()) best[i] = b1;
+			else if(!b1.valid() && b2.valid()) best[i] = b2;
+			else if(!b1.valid() && !b2.valid()) return -10;
+			else{
+				if(b1.count<b2.count){
+					best[i] = b1;
+				}else {
+					best[i] = b2;
+				}
+			}
+			return best[i].count;
+		}else{
+			if(i!=call.size() && cur->hasKey(ch)) cur = (*cur)[ch];
+			else return -10;
 		}
 	}
-	return ret;
+	return -5;
 }
+#else
+int FindPart(string& call,string::size_type start,TrieNode* cur, vector<BestNode>& best,int layer = 0)
+{
+	string::size_type i;
+	cerr<<"Into layer "<<layer<<endl;
+	for(i = start;i < call.size()+1;i++){
+		char ch = call[0];
+		if(i < call.size()) ch = call[i];
+		try{
+			if(ch<'0'&&ch>'9') throw ch;
+		}catch(char){
+			cerr<<"ERROR!!!!:bad ch:"<<ch<<endl;
+		}
+		if(cur->hasWord()){
+			if(i!=call.size() && best[i].valid()){
+				cerr<<"pre stored, return "<<best[i].count+1<<endl;
+				return best[i].count+1;
+			}else if(i==call.size()){
+				cerr<<"reach end, last word: "<<cur->word<<endl;
+				return 1;
+			}
+			BestNode b1,b2;
+			if(cur->hasKey(ch)){
+				cerr<<"Route 1 continue at "<<ch<<endl;
+				b1.node = (*cur)[ch];
+				b1.rollback = false;
+				b1.count = FindPart(call,i+1,b1.node,best,layer+1);
+				cerr<<"Route 1 done, b1 = "<<b1.count<<endl;
+			}
+			if(cur->root->hasKey(ch)){
+				cerr<<"Route 2 start at "<<ch<<" word = "<<cur->word<<endl;
+				b2.node = (*(cur->root))[ch];
+				b2.rollback = true;
+				b2.count = FindPart(call,i+1,b2.node,best,layer+1)+1;
+				cerr<<"Route 2 done, b1 = "<<b2.count<<endl;
+			}
+			if(b1.valid() && !b2.valid()) best[i] = b1;
+			else if(!b1.valid() && b2.valid()) best[i] = b2;
+			else if(!b1.valid() && !b2.valid()){
+				cerr<<"None valid branch, return -10"<<endl;
+				return -10;
+			}else{
+				cerr<<"Comparing b1 and b2"<<endl;
+				cerr<<"b1 has count: "<<b1.count<<endl;
+				cerr<<"b2 has count: "<<b2.count<<endl;
+				if(b1.count<b2.count){
+					best[i] = b1;
+					cerr<<"At "<<layer<<" best is continue, count = "<<best[i].count<<endl;
+				}else {
+					best[i] = b2;
+					cerr<<"At "<<layer<<" best is end, count = "<<best[i].count<<endl;
+				}
+			}
+			cerr<<"return "<<best[i].count<<endl;
+			return best[i].count;
+		}else{
+			if(i!=call.size() && cur->hasKey(ch)){
+				cerr<<"next: "<<ch<<endl;
+				cur = (*cur)[ch];
+			}else {
+				cerr<<"Dead End, No word available"<<endl;
+				return -10;
+			}
+		}
+	}
+	return -10;
+}
+#endif
 
-Memo* Memo::rnext(const char ch)
-{
-	if(rptr==nullptr) return nullptr;
-	Memo* ret = nullptr;
-	if( rptr->hasKey(ch) ){
-		ret = new Memo(*this);
-		ret->rptr = (*rptr)[ch];
-		ret->str.front() = rptr->word;
-	} else {
-		if(str.front().empty()) 
-			return nullptr;
-		else{
-			if( rptr->root->hasKey(ch) ){
-				ret = new Memo(*this);
-				ret->rptr = (*(rptr->root))[ch];
-				ret->str.push_front(rptr->word);
-			}
-			else
-				return nullptr;
-		}
-	}
-	return ret;
-}
-// DP:
-// Trie Tree and Reverse Trie Tree
-// if a word exists, one possible seq appears.
-// memo table is marked by the start and end of each sub seq.
-// A shortest seq is seq[i:k]+seq[k+1,j] where two seq are both shortest, or a total new seq
-// use the two trie trees, search forward and backword for valid words
 string FindSeq(string& call,list<string>& wordlist)
 {
 	string ret;
-	string::size_type i,j,x,y;
-
-	Memo*** memo = new Memo** [call.size()];
-	memo[0] = new Memo*[call.size() * call.size()]();
-	for(int k = 1;k < call.size();k++)
-		memo[k] = memo[k-1]+call.size();
+	string::size_type i;
+	vector<BestNode> best(call.size());
 	TrieNode* root = TrieNode::buildTree(wordlist);
-	TrieNode* revroot = TrieNode::buildRevTree(wordlist);
-	for(i = 0; i < call.size(); i++){
-		memo[i][i] = Memo(root,nullptr).next(call[i]);
-		if(revroot->hasKey(call[i])) memo[i][i].rptr = revroot[call[i]];
-	}
-	for(x = 1; x < call.size(); x++){ // start of substring
-		for(y = 0; y+x < call.size(); y++){ //end of substring
-			i = x+y;
-			j = y;
-			Memo* m1 = memo[i-1][j]->next(call[j]);
-			Memo* m2 = memo[i][j+1]->rnext(call[i]);
-			// nullptr is error state, which means the whole branch fails and should be discarded.
-			// empty string means this branch has not get the end value, it does not implies the branch is invalid.
-			// but if a branch has return a string, it is sure that the branch is still working.
-			if(m1 && m2)
-				if(memo[i-1][j] < memo[i][j-1]){
-					memo[i][j].str.push_back(s1);
+	int count = FindPart(call,0,root,best);
+	#ifdef DEBUG
+	cerr<<"Done, count = "<<count<<endl;
+	#endif
+	list<string> retls;
+	if(count < 0) return "No solution.";
+	else{
+		int last = 0;
+		TrieNode* lastptr;
+		try{
+			#ifdef DEBUG 
+			cerr<<endl<<call<<endl<<"Parse done, output:"<<endl;
+			#endif
+			for(i=0;i<call.size();i++){
+				lastptr = root;
+				#ifdef DEBUG 
+				cerr<<i<<": "<<call[i]<<endl;
+				#endif
+				if(root->hasWord()&&best[i].valid()){
+					if(best[i].rollback){
+						retls.push_back(root->word);
+						root = (*(root->root))[call[i]];
+					}else{
+						root = (*root)[call[i]];
+					}
+				}else if(!root->hasWord()){
+					root = (*root)[call[i]];
 				}else{
-					memo[i][j].str.push_front(s2);
+					throw root;
 				}
-			}else if(!m1 && m2){
-				memo[i][j].str.push_front(s2);
-			}else if(s2.empty() && !s1.empty()){
-				memo[i][j].str.push_back(s1);
+				if(root == nullptr) throw root;
 			}
+			retls.push_back(root->word);
+		}catch(...){
+			cerr<<"last "<<last<<endl;
+			if(last==1){
+				cerr<<"Error!"<<endl;
+				cerr<<"validity: "<<best[i].valid()<<endl;
+				cerr<<"word: "<<lastptr->word.empty()<<endl;
+				if(lastptr->word.empty())
+					cout<<"last word empty, show pointer: "<<lastptr->hasKey(call[i-1])<<endl;
+				cerr<<"pointer: "<<lastptr<<endl;
+			}
+			cerr<<root<<endl;
+			cerr<<best[i].rollback<<"  "<<best[i].valid()<<endl;
+			cerr<<call<<": "<<i<<endl;
 		}
 	}
-	if(memo[call.size()-1][0].ptr==root || memo[call.size()-1][0].rptr == root){
-		auto it = memo[call.size()-1][0].str.begin();
-		for(it;it!=memo[call.size()-1][0].str.end();it++)
-			ret += (*it + " ");
-		if(!ret.empty()) ret = ret.substr(0,ret.size()-1);
-	} else {
-		ret = "No solution.";
+	for(auto it = retls.begin();it!=retls.end();it++){
+		ret+=*it;
+		ret+=' ';
 	}
+	ret = ret.substr(0,ret.size()-1);
 	root->flush();
-	revroot->flush();
-	for(auto ita = memo.begin();ita != memo.end();ita++){
-		for(auto itb = ita->begin(); itb != ita->end(); itb++){
-			if( *itb != nullptr) delete *itb;
-		}
-	}
-	delete [] memo[0];
-	delete [] memo;
 	return ret;
 }
+
 
 int main()
 {
 	maptable['i'] = '1';maptable['j'] = '1';
 	maptable['a'] = '2';maptable['b'] = '2';maptable['c'] = '2';
-	maptable['d'] = '3';maptable['e'] = '3';maptable['d'] = '3';
+	maptable['d'] = '3';maptable['e'] = '3';maptable['f'] = '3';
 	maptable['g'] = '4';maptable['h'] = '4';
 	maptable['k'] = '5';maptable['l'] = '5';
 	maptable['m'] = '6';maptable['n'] = '6';
@@ -232,6 +264,9 @@ int main()
 	while(true){
 		cin >> call;
 		if(call=="-1") break;
+		#ifdef DEBUG
+		cerr<<endl<<"New Call: "<<call<<endl;
+		#endif
 		cin >> num;
 		for(int i = 0;i<num;i++){
 			cin >> str;
